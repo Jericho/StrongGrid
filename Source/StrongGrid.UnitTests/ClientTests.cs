b9366e5c8b80e324010e079552af1e5b58a71778
Jeremie Desautels <desautelsj@hotmail.com>
2016-10-16 15:58:08 -0500

    Add the Client class

---
+++ Source/StrongGrid.UnitTests/ClientTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StrongGrid.UnitTests
+{
+	[TestClass]
+	public class ClientTests
+	{
+		private const string API_KEY = "my_api_key";
+
+		[TestMethod]
+		public void Version_is_not_empty()
+		{
+			// Arrange
+			var client = new Client(API_KEY);
+
+			// Act
+			var result = client.Version;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+	}
+}
