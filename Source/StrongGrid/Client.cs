dbbb579d136810afface1162e1b15a0a690507e0
Jeremie Desautels <desautelsj@hotmail.com>
2016-10-26 15:25:55 -0500

    Add the Batches resource

--- Source/StrongGrid/Client.cs
+++ Source/StrongGrid/Client.cs
@@ -32,6 +32,7 @@ namespace StrongGrid
 
 		public Alerts Alerts { get; private set; }
 		public ApiKeys ApiKeys { get; private set; }
+		public Batches Batches { get; private set; }
 		public Blocks Blocks { get; private set; }
 		public Campaigns Campaigns { get; private set; }
 		public Categories Categories { get; private set; }
@@ -68,6 +69,7 @@ namespace StrongGrid
 
 			Alerts = new Alerts(this);
 			ApiKeys = new ApiKeys(this);
+			Batches = new Batches(this);
 			Blocks = new Blocks(this);
 			Campaigns = new Campaigns(this);
 			Categories = new Categories(this);
---
+++ Source/StrongGrid/Model/BatchStatus.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace StrongGrid.Model
+{
+	public enum BatchStatus
+	{
+//		[Description("pause")]
+		[EnumMember(Value = "pause")]
+
+		Paused,
+//		[Description("cancel")]
+		[EnumMember(Value = "cancel")]
+		Canceled
+	}
+}
