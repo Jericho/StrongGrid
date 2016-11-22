6243665adf9de99207be00a00da4e7472e161743
Jeremie Desautels <desautelsj@hotmail.com>
2016-11-22 14:13:16 -0600

    Fixes #78

---
+++ Source/StrongGrid/Model/BccSettings.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace StrongGrid.Model
+{
+	public class BccSettings
+	{
+		[JsonProperty("enable")]
+		public bool Enabled { get; set; }
+
+		[JsonProperty("email")]
+		public string EmailAddress { get; set; }
+	}
+}
