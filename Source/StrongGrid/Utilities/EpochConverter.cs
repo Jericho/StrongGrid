a09cfa97c051742779c9d93ae948ac6ccac321c4
Jeremie Desautels <desautelsj@hotmail.com>
2016-11-27 22:06:48 -0600

    More xml comments (#76)

--- Source/StrongGrid/Utilities/EpochConverter.cs
+++ Source/StrongGrid/Utilities/EpochConverter.cs
@@ -4,15 +4,32 @@ using System;
 
 namespace StrongGrid.Utilities
 {
+	/// <summary>
+	/// Converts a 'Unix time' expressed as the number of seconds since midnight on January 1st 1970 to and from JSON.
+	/// </summary>
+	/// <seealso cref="Newtonsoft.Json.Converters.DateTimeConverterBase" />
 	public class EpochConverter : DateTimeConverterBase
 	{
 		private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
+		/// <summary>
+		/// Determines whether this instance can convert the specified object type.
+		/// </summary>
+		/// <param name="objectType">Type of the object.</param>
+		/// <returns>
+		/// <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+		/// </returns>
 		public override bool CanConvert(Type objectType)
 		{
 			return objectType == typeof(DateTime);
 		}
 
+		/// <summary>
+		/// Writes the JSON representation of the object.
+		/// </summary>
+		/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
+		/// <param name="value">The value.</param>
+		/// <param name="serializer">The calling serializer.</param>
 		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
 		{
 			if (value == null) return;
@@ -21,6 +38,16 @@ namespace StrongGrid.Utilities
 			serializer.Serialize(writer, secondsSinceEpoch);
 		}
 
+		/// <summary>
+		/// Reads the JSON representation of the object.
+		/// </summary>
+		/// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
+		/// <param name="objectType">Type of the object.</param>
+		/// <param name="existingValue">The existing value of object being read.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		/// <returns>
+		/// The object value.
+		/// </returns>
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
 			if (reader.Value == null) return null;
--- Source/StrongGrid/Utilities/IntegerBooleanConverter.cs
+++ Source/StrongGrid/Utilities/IntegerBooleanConverter.cs
@@ -3,13 +3,30 @@ using System;
 
 namespace StrongGrid.Utilities
 {
+	/// <summary>
+	/// Converts a boolean expressed as  1 for True or 0 for False to and from JSON.
+	/// </summary>
+	/// <seealso cref="Newtonsoft.Json.JsonConverter" />
 	public class IntegerBooleanConverter : JsonConverter
 	{
+		/// <summary>
+		/// Determines whether this instance can convert the specified object type.
+		/// </summary>
+		/// <param name="objectType">Type of the object.</param>
+		/// <returns>
+		/// <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+		/// </returns>
 		public override bool CanConvert(Type objectType)
 		{
 			return objectType == typeof(bool);
 		}
 
+		/// <summary>
+		/// Writes the JSON representation of the object.
+		/// </summary>
+		/// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
+		/// <param name="value">The value.</param>
+		/// <param name="serializer">The calling serializer.</param>
 		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
 		{
 			if (value == null) return;
@@ -17,6 +34,16 @@ namespace StrongGrid.Utilities
 			writer.WriteValue(((bool)value) ? 1 : 0);
 		}
 
+		/// <summary>
+		/// Reads the JSON representation of the object.
+		/// </summary>
+		/// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
+		/// <param name="objectType">Type of the object.</param>
+		/// <param name="existingValue">The existing value of object being read.</param>
+		/// <param name="serializer">The calling serializer.</param>
+		/// <returns>
+		/// The object value.
+		/// </returns>
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
 			if (reader.Value == null) return null;
