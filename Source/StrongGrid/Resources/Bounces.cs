63b5de53d49ea8999d522f7412aeeff93503ff91
Jeremie Desautels <desautelsj@hotmail.com>
2017-03-16 09:25:54 -0500

    Bounces.GetAsync renamed to GetAllAsync

--- Source/StrongGrid/Resources/Bounces.cs
+++ Source/StrongGrid/Resources/Bounces.cs
@@ -31,7 +31,7 @@ namespace StrongGrid.Resources
 		}
 
 		/// <summary>
-		/// Get a list of bounces
+		/// Get all bounces
 		/// </summary>
 		/// <param name="start">The start.</param>
 		/// <param name="end">The end.</param>
@@ -39,7 +39,7 @@ namespace StrongGrid.Resources
 		/// <returns>
 		/// An array of <see cref="Bounce" />.
 		/// </returns>
-		public Task<Bounce[]> GetAsync(DateTime? start = null, DateTime? end = null, CancellationToken cancellationToken = default(CancellationToken))
+		public Task<Bounce[]> GetAllAsync(DateTime? start = null, DateTime? end = null, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			return _client
 				.GetAsync(_endpoint)
