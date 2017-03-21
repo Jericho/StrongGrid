74207fe0cc70840211a208fce8074ee950d7d2c5
Jeremie Desautels <desautelsj@hotmail.com>
2017-03-21 18:28:54 -0400

    Add unit tests for the WebhookStats resource

--- Source/StrongGrid.UnitTests/Resources/StatisticsTests.cs
+++ Source/StrongGrid.UnitTests/Resources/StatisticsTests.cs
@@ -26,26 +26,26 @@ namespace StrongGrid.Resources.UnitTests
 			var endDate = new DateTime(2015, 1, 2);
 			var apiResponse = @"[
 				{
-					'date': '2015 - 01 - 01',
+					'date': '2015-01-01',
 					'stats': [
 						{
-						'metrics': {
-							'blocks': 1,
-							'bounce_drops': 0,
-							'bounces': 0,
-							'clicks': 0,
-							'deferred': 1,
-							'delivered': 1,
-							'invalid_emails': 1,
-							'opens': 1,
-							'processed': 2,
-							'requests': 3,
-							'spam_report_drops': 0,
-							'spam_reports': 0,
-							'unique_clicks': 0,
-							'unique_opens': 1,
-							'unsubscribe_drops': 0,
-							'unsubscribes': 0
+							'metrics': {
+								'blocks': 1,
+								'bounce_drops': 0,
+								'bounces': 0,
+								'clicks': 0,
+								'deferred': 1,
+								'delivered': 1,
+								'invalid_emails': 1,
+								'opens': 1,
+								'processed': 2,
+								'requests': 3,
+								'spam_report_drops': 0,
+								'spam_reports': 0,
+								'unique_clicks': 0,
+								'unique_opens': 1,
+								'unsubscribe_drops': 0,
+								'unsubscribes': 0
 							}
 						}
 					]
@@ -54,23 +54,23 @@ namespace StrongGrid.Resources.UnitTests
 					'date': '2015-01-02',
 					'stats': [
 						{
-						'metrics': {
-							'blocks': 0,
-							'bounce_drops': 0,
-							'bounces': 0,
-							'clicks': 0,
-							'deferred': 0,
-							'delivered': 0,
-							'invalid_emails': 0,
-							'opens': 0,
-							'processed': 0,
-							'requests': 0,
-							'spam_report_drops': 0,
-							'spam_reports': 0,
-							'unique_clicks': 0,
-							'unique_opens': 0,
-							'unsubscribe_drops': 0,
-							'unsubscribes': 0
+							'metrics': {
+								'blocks': 0,
+								'bounce_drops': 0,
+								'bounces': 0,
+								'clicks': 0,
+								'deferred': 0,
+								'delivered': 0,
+								'invalid_emails': 0,
+								'opens': 0,
+								'processed': 0,
+								'requests': 0,
+								'spam_report_drops': 0,
+								'spam_reports': 0,
+								'unique_clicks': 0,
+								'unique_opens': 0,
+								'unsubscribe_drops': 0,
+								'unsubscribes': 0
 							}
 						}
 					]
---
+++ Source/StrongGrid.UnitTests/Resources/WebhookStatsTests.cs
@@ -0,0 +1,67 @@
+﻿using RichardSzalay.MockHttp;
+using Shouldly;
+using StrongGrid.Model;
+using StrongGrid.UnitTests;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using Xunit;
+
+namespace StrongGrid.Resources.UnitTests
+{
+	public class WebhookStatsTests
+	{
+		[Fact]
+		public void GetGlobalStats()
+		{
+			// Arrange
+			var startDate = new DateTime(2015, 1, 1);
+			var endDate = new DateTime(2015, 1, 2);
+			var apiResponse = @"[
+				{
+					'date': '2015-01-01',
+					'stats': [
+						{
+							'metrics': {
+								'received': 1
+							}
+						}
+					]
+				},
+				{
+					'date': '2015-01-02',
+					'stats': [
+						{
+							'metrics': {
+								'received': 3
+							}
+						}
+					]
+				}
+			]";
+
+			var mockHttp = new MockHttpMessageHandler();
+			mockHttp.Expect(HttpMethod.Get, Utils.GetSendGridApiUri($"user/webhooks/parse/stats?start_date={startDate.ToString("yyyy-MM-dd")}&end_date={ endDate.ToString("yyyy-MM-dd")}")).Respond("application/json", apiResponse);
+
+			var client = Utils.GetFluentClient(mockHttp);
+			var webhookStats = new WebhookStats(client);
+
+			// Act
+			var result = webhookStats.GetInboundParseUsageAsync(startDate, endDate, AggregateBy.None, CancellationToken.None).Result;
+
+			// Assert
+			mockHttp.VerifyNoOutstandingExpectation();
+			mockHttp.VerifyNoOutstandingRequest();
+			result.ShouldNotBeNull();
+			result.Length.ShouldBe(2);
+			result[0].Stats.Length.ShouldBe(1);
+
+			result[0].Stats[0].Metrics.Length.ShouldBe(1);
+			result[0].Stats[0].Metrics.Single(m => m.Key == "received").Value.ShouldBe(1);
+
+			result[1].Stats[0].Metrics.Length.ShouldBe(1);
+			result[1].Stats[0].Metrics.Single(m => m.Key == "received").Value.ShouldBe(3);
+		}
+	}
+}
