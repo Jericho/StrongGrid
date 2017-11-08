d9824af47ecf5038b61e52aa67dbd13953569c54
Jeremie Desautels <desautelsj@hotmail.com>
2017-11-08 13:56:39 -0500

    Add OnBehalfOf to InvalidEmails

--- Source/StrongGrid.IntegrationTests/Program.cs
+++ Source/StrongGrid.IntegrationTests/Program.cs
@@ -812,7 +812,7 @@ namespace StrongGrid.IntegrationTests
 			var startDate = new DateTime(lastYear, 1, 1, 0, 0, 0);
 			var endDate = new DateTime(thisYear, 12, 31, 23, 59, 59);
 
-			var invalidEmails = await client.InvalidEmails.GetAllAsync(startDate, endDate, 25, 0, cancellationToken).ConfigureAwait(false);
+			var invalidEmails = await client.InvalidEmails.GetAllAsync(startDate, endDate, 25, 0, null, cancellationToken).ConfigureAwait(false);
 			await log.WriteLineAsync($"All invalid emails retrieved. There are {invalidEmails.Length} invalid email addresses in {lastYear} and {thisYear}").ConfigureAwait(false);
 		}
 
