661eecd750d335b2f067265443df8f9cb96e8e3a
Jeremie Desautels <desautelsj@hotmail.com>
2018-04-02 18:52:05 -0400

    Add the RequestCsvAsync, GetCsvDownloadUrlAsync and DownloadCsvAsync methods to the EmailActivities resource

--- Source/StrongGrid/Resources/EmailActivities.cs
+++ Source/StrongGrid/Resources/EmailActivities.cs
@@ -1,6 +1,8 @@
 ﻿using Pathoschild.Http.Client;
 using StrongGrid.Models;
 using StrongGrid.Utilities;
+using System.IO;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -35,7 +37,7 @@ namespace StrongGrid.Resources
 		/// <returns>
 		/// An array of <see cref="EmailMessageActivity" />.
 		/// </returns>
-		public Task<EmailMessageActivity[]> SearchMessages(int limit = 20, CancellationToken cancellationToken = default(CancellationToken))
+		public Task<EmailMessageActivity[]> SearchMessagesAsync(int limit = 20, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			return _client
 				.GetAsync(_endpoint)
@@ -52,12 +54,89 @@ namespace StrongGrid.Resources
 		/// <returns>
 		/// An array of <see cref="EmailMessageSummary" />.
 		/// </returns>
-		public Task<EmailMessageSummary> GetMessageSummary(string messageId, CancellationToken cancellationToken = default(CancellationToken))
+		public Task<EmailMessageSummary> GetMessageSummaryAsync(string messageId, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			return _client
 				.GetAsync($"{_endpoint}/{messageId}")
 				.WithCancellationToken(cancellationToken)
 				.AsSendGridObject<EmailMessageSummary>();
+		}
+
+		/// <summary>
+		/// This request kicks of a process to generate a CSV file. When the file is
+		/// generated, the email that is listed as the account owner gets an email
+		/// that links out to the file that is ready for download. The link expires
+		/// in 3 days.
+		/// </summary>
+		/// <remarks>
+		/// The CSV fill contain the last 1 million messages.This endpoint
+		/// will be rate limited to 1 request every 12 hours.
+		/// </remarks>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The async task.
+		/// </returns>
+		public Task RequestCsvAsync(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return _client
+				.PostAsync($"{_endpoint}/download")
+				.WithCancellationToken(cancellationToken)
+				.AsMessage();
+		}
+
+		/// <summary>
+		/// Get the URL where the CSV can be downloaded.
+		/// </summary>
+		/// <param name="downloadUUID">UUID used to locate the download CSV request entry. You can find this UUID in the email that is sent with the POST Request a CSV.</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The URL as a string.
+		/// </returns>
+		public Task<string> GetCsvDownloadUrlAsync(string downloadUUID, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return _client
+				.GetAsync($"{_endpoint}/download/{downloadUUID}")
+				.WithCancellationToken(cancellationToken)
+				.AsSendGridObject<string>("presigned_url");
+		}
+
+		/// <summary>
+		/// Download the CSV as a stream.
+		/// </summary>
+		/// <param name="downloadUUID">UUID used to locate the download CSV request entry. You can find this UUID in the email that is sent with the POST Request a CSV.</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The stream.
+		/// </returns>
+		public async Task<Stream> DownloadCsvAsync(string downloadUUID, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			var url = await GetCsvDownloadUrlAsync(downloadUUID, cancellationToken);
+
+			using (var client = new HttpClient())
+			{
+				var responseStream = await client.GetStreamAsync(url).ConfigureAwait(false);
+				return responseStream;
+			}
+		}
+
+		/// <summary>
+		/// Download the CSV and save it to a file.
+		/// </summary>
+		/// <param name="downloadUUID">UUID used to locate the download CSV request entry. You can find this UUID in the email that is sent with the POST Request a CSV.</param>
+		/// <param name="destinationPath">The path and name of the CSV file</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The async task.
+		/// </returns>
+		public async Task DownloadCsvAsync(string downloadUUID, string destinationPath, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			using (var responseStream = await DownloadCsvAsync(downloadUUID, cancellationToken).ConfigureAwait(false))
+			{
+				using (Stream output = File.OpenWrite(destinationPath))
+				{
+					responseStream.CopyTo(output);
+				}
+			}
 		}
 	}
 }
--- Source/StrongGrid/Resources/IEmailActivities.cs
+++ Source/StrongGrid/Resources/IEmailActivities.cs
@@ -1,4 +1,5 @@
 ﻿using StrongGrid.Models;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,7 +22,7 @@ namespace StrongGrid.Resources
 		/// <returns>
 		/// An array of <see cref="EmailMessageActivity" />.
 		/// </returns>
-		Task<EmailMessageActivity[]> SearchMessages(int limit = 20, CancellationToken cancellationToken = default(CancellationToken));
+		Task<EmailMessageActivity[]> SearchMessagesAsync(int limit = 20, CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		/// Get all of the details about the specified message.
@@ -31,6 +32,53 @@ namespace StrongGrid.Resources
 		/// <returns>
 		/// The <see cref="EmailMessageSummary" />.
 		/// </returns>
-		Task<EmailMessageSummary> GetMessageSummary(string messageId, CancellationToken cancellationToken = default(CancellationToken));
+		Task<EmailMessageSummary> GetMessageSummaryAsync(string messageId, CancellationToken cancellationToken = default(CancellationToken));
+
+		/// <summary>
+		/// This request kicks of a process to generate a CSV file. When the file is
+		/// generated, the email that is listed as the account owner gets an email
+		/// that links out to the file that is ready for download. The link expires
+		/// in 3 days.
+		/// </summary>
+		/// <remarks>
+		/// The CSV fill contain the last 1 million messages.This endpoint
+		/// will be rate limited to 1 request every 12 hours.
+		/// </remarks>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The async task.
+		/// </returns>
+		Task RequestCsvAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+		/// <summary>
+		/// Get the URL where the CSV can be downloaded.
+		/// </summary>
+		/// <param name="downloadUUID">UUID used to locate the download CSV request entry. You can find this UUID in the email that is sent with the POST Request a CSV.</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The URL as a string.
+		/// </returns>
+		Task<string> GetCsvDownloadUrlAsync(string downloadUUID, CancellationToken cancellationToken = default(CancellationToken));
+
+		/// <summary>
+		/// Download the CSV as a stream.
+		/// </summary>
+		/// <param name="downloadUUID">UUID used to locate the download CSV request entry. You can find this UUID in the email that is sent with the POST Request a CSV.</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The stream.
+		/// </returns>
+		Task<Stream> DownloadCsvAsync(string downloadUUID, CancellationToken cancellationToken = default(CancellationToken));
+
+		/// <summary>
+		/// Download the CSV and save it to a file.
+		/// </summary>
+		/// <param name="downloadUUID">UUID used to locate the download CSV request entry. You can find this UUID in the email that is sent with the POST Request a CSV.</param>
+		/// <param name="destinationPath">The path and name of the CSV file</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>
+		/// The async task.
+		/// </returns>
+		Task DownloadCsvAsync(string downloadUUID, string destinationPath, CancellationToken cancellationToken = default(CancellationToken));
 	}
 }
