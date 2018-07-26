1e69f0027e26b1a516d03467b24d95762f7117c4
Jeremie Desautels <desautelsj@hotmail.com>
2018-07-26 14:27:45 -0400

    Add the 'Type' property to the Template model class

---
+++ Source/StrongGrid/Models/TemplateType.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace StrongGrid.Models
+{
+	/// <summary>
+	/// Enumeration to indicate the type of template.
+	/// </summary>
+	[JsonConverter(typeof(StringEnumConverter))]
+	public enum TemplateType
+	{
+		/// <summary>
+		/// The old type of template.
+		/// </summary>
+		[EnumMember(Value = "legacy")]
+		Legacy,
+
+		/// <summary>
+		/// The new dynamic type of template.
+		/// </summary>
+		[EnumMember(Value = "dynamic")]
+		Dynamic
+	}
+}
