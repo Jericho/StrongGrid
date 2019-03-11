833d5ad9db832976565927076f1189dd19588c95
Jeremie Desautels <desautelsj@hotmail.com>
2019-03-11 16:19:39 -0400

    Add ability to convert a single object in EmailActivityEventConverter
    Resolves #262

--- Source/StrongGrid/Utilities/EmailActivityEventConverter.cs
+++ Source/StrongGrid/Utilities/EmailActivityEventConverter.cs
@@ -74,65 +74,70 @@ namespace StrongGrid.Utilities
 		{
 			if (reader.TokenType == JsonToken.StartArray)
 			{
-				var events = new List<Event>();
 				var jArray = JArray.Load(reader);
+				var items = jArray
+					.OfType<JObject>()
+					.Select(item => Convert(item, serializer))
+					.Where(item => item != null)
+					.ToArray();
 
-				foreach (JObject jsonObject in jArray)
-				{
-					jsonObject.TryGetValue("event_name", StringComparison.OrdinalIgnoreCase, out JToken eventTypeJsonProperty);
-					var eventType = (EventType)eventTypeJsonProperty.ToObject(typeof(EventType));
-
-					var emailActivityEvent = (Event)null;
-					switch (eventType)
-					{
-						case EventType.Bounce:
-							emailActivityEvent = jsonObject.ToObject<BounceEvent>(serializer);
-							break;
-						case EventType.Open:
-							emailActivityEvent = jsonObject.ToObject<OpenEvent>(serializer);
-							break;
-						case EventType.Click:
-							emailActivityEvent = jsonObject.ToObject<ClickEvent>(serializer);
-							break;
-						case EventType.Processed:
-							emailActivityEvent = jsonObject.ToObject<ProcessedEvent>(serializer);
-							break;
-						case EventType.Dropped:
-							emailActivityEvent = jsonObject.ToObject<DroppedEvent>(serializer);
-							break;
-						case EventType.Delivered:
-							emailActivityEvent = jsonObject.ToObject<DeliveredEvent>(serializer);
-							break;
-						case EventType.Deferred:
-							emailActivityEvent = jsonObject.ToObject<DeferredEvent>(serializer);
-							break;
-						case EventType.SpamReport:
-							emailActivityEvent = jsonObject.ToObject<SpamReportEvent>(serializer);
-							break;
-						case EventType.Unsubscribe:
-							emailActivityEvent = jsonObject.ToObject<UnsubscribeEvent>(serializer);
-							break;
-						case EventType.GroupUnsubscribe:
-							emailActivityEvent = jsonObject.ToObject<GroupUnsubscribeEvent>(serializer);
-							break;
-						case EventType.GroupResubscribe:
-							emailActivityEvent = jsonObject.ToObject<GroupResubscribeEvent>(serializer);
-							break;
-						default:
-							throw new Exception($"{eventTypeJsonProperty.ToString()} is an unknown event type");
-					}
-
-					if (emailActivityEvent != null) events.Add(emailActivityEvent);
-				}
-
-				return events.ToArray();
+				return items;
+			}
+			else if (reader.TokenType == JsonToken.StartObject)
+			{
+				var jObject = JObject.Load(reader);
+				return Convert(jObject, serializer);
 			}
 
-			/*
-				When we stop supporting .NET 4.5.2 we will be able to use the following:
-				return Array.Empty<Event>();
-			*/
-			return Enumerable.Empty<Field>().ToArray();
+			throw new Exception("Unable to convert to Event");
+		}
+
+		private Event Convert(JObject jsonObject, JsonSerializer serializer)
+		{
+			jsonObject.TryGetValue("event_name", StringComparison.OrdinalIgnoreCase, out JToken eventTypeJsonProperty);
+			var eventType = (EventType)eventTypeJsonProperty.ToObject(typeof(EventType));
+
+			var emailActivityEvent = (Event)null;
+			switch (eventType)
+			{
+				case EventType.Bounce:
+					emailActivityEvent = jsonObject.ToObject<BounceEvent>(serializer);
+					break;
+				case EventType.Open:
+					emailActivityEvent = jsonObject.ToObject<OpenEvent>(serializer);
+					break;
+				case EventType.Click:
+					emailActivityEvent = jsonObject.ToObject<ClickEvent>(serializer);
+					break;
+				case EventType.Processed:
+					emailActivityEvent = jsonObject.ToObject<ProcessedEvent>(serializer);
+					break;
+				case EventType.Dropped:
+					emailActivityEvent = jsonObject.ToObject<DroppedEvent>(serializer);
+					break;
+				case EventType.Delivered:
+					emailActivityEvent = jsonObject.ToObject<DeliveredEvent>(serializer);
+					break;
+				case EventType.Deferred:
+					emailActivityEvent = jsonObject.ToObject<DeferredEvent>(serializer);
+					break;
+				case EventType.SpamReport:
+					emailActivityEvent = jsonObject.ToObject<SpamReportEvent>(serializer);
+					break;
+				case EventType.Unsubscribe:
+					emailActivityEvent = jsonObject.ToObject<UnsubscribeEvent>(serializer);
+					break;
+				case EventType.GroupUnsubscribe:
+					emailActivityEvent = jsonObject.ToObject<GroupUnsubscribeEvent>(serializer);
+					break;
+				case EventType.GroupResubscribe:
+					emailActivityEvent = jsonObject.ToObject<GroupResubscribeEvent>(serializer);
+					break;
+				default:
+					throw new Exception($"{eventTypeJsonProperty.ToString()} is an unknown event type");
+			}
+
+			return emailActivityEvent;
 		}
 	}
 }
