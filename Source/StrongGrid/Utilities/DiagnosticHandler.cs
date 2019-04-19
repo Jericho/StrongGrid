9a71b170ca85cac2a3157e15fa41e51a9650a3f8
Jeremie Desautels <desautelsj@hotmail.com>
2019-04-19 16:54:37 -0400

    Log diagnostic even when exception occurs
    Resolves #270

--- Source/StrongGrid/Utilities/DiagnosticHandler.cs
+++ Source/StrongGrid/Utilities/DiagnosticHandler.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace StrongGrid.Utilities
@@ -41,36 +43,45 @@ namespace StrongGrid.Utilities
 		/// <param name="httpErrorAsException">Whether HTTP error responses should be raised as exceptions.</param>
 		public void OnResponse(IResponse response, bool httpErrorAsException)
 		{
-			var diagnosticMessage = string.Empty;
 			var responseTimestamp = Stopwatch.GetTimestamp();
+			var diagnostic = new StringBuilder();
 
 			try
 			{
-				var diagnosticStringBuilder = new StringBuilder();
-
 				var httpRequest = response.Message.RequestMessage;
-				diagnosticStringBuilder.AppendLine($"Request: {httpRequest}");
-				diagnosticStringBuilder.AppendLine($"Request Content: {httpRequest.Content?.ReadAsStringAsync(null).GetAwaiter().GetResult() ?? "<NULL>"}");
-
 				var httpResponse = response.Message;
-				diagnosticStringBuilder.AppendLine($"Response: {httpResponse}");
-				diagnosticStringBuilder.AppendLine($"Response.Content: {httpResponse.Content?.ReadAsStringAsync(null).GetAwaiter().GetResult() ?? "<NULL>"}");
 
+				// Log the request
+				diagnostic.AppendLine("REQUEST:");
+				diagnostic.AppendLine($"  {httpRequest.Method.Method} {httpRequest.RequestUri}");
+				LogHeaders(diagnostic, httpRequest.Headers);
+				LogContent(diagnostic, httpRequest.Content);
+
+				// Log the response
+				diagnostic.AppendLine();
+				diagnostic.AppendLine("RESPONSE:");
+				diagnostic.AppendLine($"  HTTP/{httpResponse.Version} {(int)httpResponse.StatusCode} {httpResponse.ReasonPhrase}");
+				LogHeaders(diagnostic, httpResponse.Headers);
+				LogContent(diagnostic, httpResponse.Content);
+
+				// Log diagnostic
 				if (httpRequest.Headers.TryGetValues(DIAGNOSTIC_TIMESTAMP_HEADER_NAME, out IEnumerable<string> timestampHeaderValues))
 				{
 					var diagnosticTimestamp = timestampHeaderValues.FirstOrDefault();
 					if (long.TryParse(diagnosticTimestamp, out long requestTimestamp))
 					{
 						var elapsed = TimeSpan.FromTicks(responseTimestamp - requestTimestamp);
-						diagnosticStringBuilder.AppendLine($"The request took {elapsed.ToDurationString()}");
+
+						diagnostic.AppendLine();
+						diagnostic.AppendLine("DIAGNOSTIC:");
+						diagnostic.AppendLine($"  The request took {elapsed.ToDurationString()}");
 					}
 				}
-
-				diagnosticMessage = diagnosticStringBuilder.ToString();
 			}
 			catch (Exception e)
 			{
-				Debug.WriteLine("{0}\r\nAN EXCEPTION OCCURED: {1}\r\n{0}", new string('=', 25), e.GetBaseException().Message);
+				Debug.WriteLine("{0}\r\nAN EXCEPTION OCCURED: {1}\r\n{0}", new string('=', 50), e.GetBaseException().Message);
+				diagnostic.AppendLine($"AN EXCEPTION OCCURED: {e.GetBaseException().Message}");
 
 				if (_logger != null && _logger.IsErrorEnabled())
 				{
@@ -79,9 +90,11 @@ namespace StrongGrid.Utilities
 			}
 			finally
 			{
+				var diagnosticMessage = diagnostic.ToString();
+
 				if (!string.IsNullOrEmpty(diagnosticMessage))
 				{
-					Debug.WriteLine("{0}\r\n{1}{0}", new string('=', 25), diagnosticMessage);
+					Debug.WriteLine("{0}\r\n{1}{0}", new string('=', 50), diagnosticMessage);
 
 					if (_logger != null && _logger.IsDebugEnabled())
 					{
@@ -89,6 +102,48 @@ namespace StrongGrid.Utilities
 							.Replace("{", "{{")
 							.Replace("}", "}}"));
 					}
+				}
+			}
+		}
+
+		#endregion
+
+		#region PRIVATE METHODS
+
+		private void LogHeaders(StringBuilder diagnostic, HttpHeaders httpHeaders)
+		{
+			if (httpHeaders != null)
+			{
+				foreach (var header in httpHeaders)
+				{
+					if (header.Key.Equals("authorization", StringComparison.OrdinalIgnoreCase))
+					{
+						diagnostic.AppendLine($"  {header.Key}: ...redacted for security reasons...");
+					}
+					else
+					{
+						diagnostic.AppendLine($"  {header.Key}: {string.Join(", ", header.Value)}");
+					}
+				}
+			}
+		}
+
+		private void LogContent(StringBuilder diagnostic, HttpContent httpContent)
+		{
+			if (httpContent != null)
+			{
+				LogHeaders(diagnostic, httpContent.Headers);
+
+				var contentLength = httpContent.Headers?.ContentLength.GetValueOrDefault(0) ?? 0;
+				if (!httpContent.Headers?.Contains("Content-Length") ?? false)
+				{
+					diagnostic.AppendLine($"  Content-Length: {contentLength}");
+				}
+
+				if (contentLength > 0)
+				{
+					diagnostic.AppendLine();
+					diagnostic.AppendLine(httpContent.ReadAsStringAsync(null).GetAwaiter().GetResult() ?? "<NULL>");
 				}
 			}
 		}
