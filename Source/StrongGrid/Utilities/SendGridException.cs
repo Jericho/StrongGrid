ab724def6310f83702750bdcfd296c14f6f46678
Daniel Edwards <daniel.edwards@esendex.com>
2019-05-02 15:16:59 +0100

    Edited Sendgrid Error handler to throw a typed Exception with status code

---
+++ Source/StrongGrid/Utilities/SendGridException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace StrongGrid.Utilities
+{
+	/// <summary>
+	/// Exception that includes both the formatted message and the status code.
+	/// </summary>
+	public class SendGridException : Exception
+	{
+		/// <summary>
+		/// The status code of the non-successful call
+		/// </summary>
+		public HttpStatusCode StatusCode { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SendGridException"/> class.
+		/// </summary>
+		/// <param name="message">The exception message</param>
+		/// <param name="statusCode">The status code of the non-successful call</param>
+		public SendGridException(string message, HttpStatusCode statusCode)
+			: base(message)
+		{
+			StatusCode = statusCode;
+		}
+	}
+}
