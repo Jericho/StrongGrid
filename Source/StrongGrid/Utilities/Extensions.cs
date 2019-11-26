10dbcf74b12440da011cee56f44c1690a85da4ea
Jericho <desautelsj@hotmail.com>
2019-11-25 19:55:20 -0500

    Unit tests for new async method

--- Source/StrongGrid/Utilities/Extensions.cs
+++ Source/StrongGrid/Utilities/Extensions.cs
@@ -422,50 +422,54 @@ namespace StrongGrid.Utilities
 		public static async Task<TResult[]> ForEachAsync<T, TResult>(this IEnumerable<T> items, Func<T, Task<TResult>> action, int maxDegreeOfParalellism)
 		{
 			var allTasks = new List<Task<TResult>>();
-			var throttler = new SemaphoreSlim(initialCount: maxDegreeOfParalellism);
-			foreach (var item in items)
+			using (var throttler = new SemaphoreSlim(initialCount: maxDegreeOfParalellism))
 			{
-				await throttler.WaitAsync();
-				allTasks.Add(
-					Task.Run(async () =>
-					{
-						try
+				foreach (var item in items)
+				{
+					await throttler.WaitAsync();
+					allTasks.Add(
+						Task.Run(async () =>
 						{
-							return await action(item).ConfigureAwait(false);
-						}
-						finally
-						{
-							throttler.Release();
-						}
-					}));
-			}
+							try
+							{
+								return await action(item).ConfigureAwait(false);
+							}
+							finally
+							{
+								throttler.Release();
+							}
+						}));
+				}
 
-			var results = await Task.WhenAll(allTasks).ConfigureAwait(false);
-			return results;
+				var results = await Task.WhenAll(allTasks).ConfigureAwait(false);
+				return results;
+			}
 		}
 
 		public static async Task ForEachAsync<T>(this IEnumerable<T> items, Func<T, Task> action, int maxDegreeOfParalellism)
 		{
 			var allTasks = new List<Task>();
-			var throttler = new SemaphoreSlim(initialCount: maxDegreeOfParalellism);
-			foreach (var item in items)
+			using (var throttler = new SemaphoreSlim(initialCount: maxDegreeOfParalellism))
 			{
-				await throttler.WaitAsync();
-				allTasks.Add(
-					Task.Run(async () =>
-					{
-						try
+				foreach (var item in items)
+				{
+					await throttler.WaitAsync();
+					allTasks.Add(
+						Task.Run(async () =>
 						{
-							await action(item).ConfigureAwait(false);
-						}
-						finally
-						{
-							throttler.Release();
-						}
-					}));
-			}
+							try
+							{
+								await action(item).ConfigureAwait(false);
+							}
+							finally
+							{
+								throttler.Release();
+							}
+						}));
+				}
 
-			await Task.WhenAll(allTasks).ConfigureAwait(false);
+				await Task.WhenAll(allTasks).ConfigureAwait(false);
+			}
 		}
 
 		/// <summary>
