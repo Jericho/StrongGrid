770d491712b7eca93f74e477d417ea602068242c
Jericho <desautelsj@hotmail.com>
2020-12-09 13:27:49 -0500

    Improve the 'Settings' integration test

--- Source/StrongGrid.IntegrationTests/Tests/Settings.cs
+++ Source/StrongGrid.IntegrationTests/Tests/Settings.cs
@@ -14,12 +14,24 @@ namespace StrongGrid.IntegrationTests.Tests
 
 			var partnerSettings = await client.Settings.GetAllPartnerSettingsAsync(25, 0, null, cancellationToken).ConfigureAwait(false);
 			await log.WriteLineAsync($"All partner settings retrieved. There are {partnerSettings.Length} settings").ConfigureAwait(false);
+			foreach (var partnerSetting in partnerSettings)
+			{
+				await log.WriteLineAsync($"  - {partnerSetting.Title}: {(partnerSetting.Enabled ? "Enabled" : "Not enabled")}").ConfigureAwait(false);
+			}
 
 			var trackingSettings = await client.Settings.GetAllTrackingSettingsAsync(25, 0, null, cancellationToken).ConfigureAwait(false);
-			await log.WriteLineAsync($"All partner tracking retrieved. There are {trackingSettings.Length} settings").ConfigureAwait(false);
+			await log.WriteLineAsync($"All tracking settings retrieved. There are {trackingSettings.Length} settings").ConfigureAwait(false);
+			foreach (var trackingSetting in trackingSettings)
+			{
+				await log.WriteLineAsync($"  - {trackingSetting.Title}:  {(trackingSetting.Enabled ? "Enabled" : "Not enabled")}").ConfigureAwait(false);
+			}
 
 			var mailSettings = await client.Settings.GetAllMailSettingsAsync(25, 0, null, cancellationToken).ConfigureAwait(false);
-			await log.WriteLineAsync($"All mail tracking retrieved. There are {mailSettings.Length} settings").ConfigureAwait(false);
+			await log.WriteLineAsync($"All mail setting retrieved. There are {mailSettings.Length} settings").ConfigureAwait(false);
+			foreach (var mailSetting in mailSettings)
+			{
+				await log.WriteLineAsync($"  - {mailSetting.Title}: {(mailSetting.Enabled ? "Enabled" : "Not enabled")}").ConfigureAwait(false);
+			}
 		}
 	}
 }
