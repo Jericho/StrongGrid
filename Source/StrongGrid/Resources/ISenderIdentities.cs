2bb38bed26841c6bba8cb7281c2964c26c20c4de
Jericho <desautelsj@hotmail.com>
2020-12-10 15:43:20 -0500

    Add the DeleteAsync, ResendVerificationAsync and UpdateAsync methods to the SenderIdentities resource

--- Source/StrongGrid/Resources/ISenderIdentities.cs
+++ Source/StrongGrid/Resources/ISenderIdentities.cs
@@ -1,4 +1,5 @@
 using StrongGrid.Models;
+using StrongGrid.Utilities;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -66,5 +67,44 @@ namespace StrongGrid.Resources
 		/// The asynchronous task.
 		/// </returns>
 		Task VerifySenderAsync(string verificationToken, CancellationToken cancellationToken = default);
+
+		/// <summary>
+		/// Delete a sender identity.
+		/// </summary>
+		/// <param name="senderIdentityId">The id assigned to the sender identity.</param>
+		/// <param name="cancellationToken">Cancellation token.</param>
+		/// <returns>
+		/// The async task.
+		/// </returns>
+		Task DeleteAsync(long senderIdentityId, CancellationToken cancellationToken = default);
+
+		/// <summary>
+		/// Resend a verification email to a specified Sender Identity.
+		/// </summary>
+		/// <param name="senderIdentityId">The id assigned to the sender identity.</param>
+		/// <param name="cancellationToken">Cancellation token.</param>
+		/// <returns>
+		/// The async task.
+		/// </returns>
+		Task ResendVerificationAsync(long senderIdentityId, CancellationToken cancellationToken = default);
+
+		/// <summary>
+		/// Update an existing sender identity.
+		/// </summary>
+		/// <param name="senderIdentityId">The sender identity identifier.</param>
+		/// <param name="nickname">The nickname.</param>
+		/// <param name="from">From.</param>
+		/// <param name="replyTo">The reply to.</param>
+		/// <param name="address1">The address1.</param>
+		/// <param name="address2">The address2.</param>
+		/// <param name="city">The city.</param>
+		/// <param name="state">The state.</param>
+		/// <param name="zip">The zip.</param>
+		/// <param name="country">The country.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>
+		/// The <see cref="SenderIdentity" />.
+		/// </returns>
+		Task<SenderIdentity> UpdateAsync(long senderIdentityId, Parameter<string> nickname = default, Parameter<MailAddress> from = default, Parameter<MailAddress> replyTo = default, Parameter<string> address1 = default, Parameter<string> address2 = default, Parameter<string> city = default, Parameter<string> state = default, Parameter<string> zip = default, Parameter<string> country = default, CancellationToken cancellationToken = default);
 	}
 }
