37549f7983c7421afdec1157678b13a20e22a6ee
Jericho <desautelsj@hotmail.com>
2021-01-18 16:28:11 -0500

    (GH-361) Add xml comments

--- Source/StrongGrid/Models/PaginationLink.cs
+++ Source/StrongGrid/Models/PaginationLink.cs
@@ -1,11 +1,26 @@
 namespace StrongGrid.Models
 {
+	/// <summary>
+	/// Pagination information.
+	/// </summary>
 	public class PaginationLink
 	{
+		/// <summary>
+		/// Gets or sets the uri to the page.
+		/// </summary>
 		public string Link { get; set; }
 
+		/// <summary>
+		/// Gets or sets the value describing the pagination link.
+		/// </summary>
+		/// <remarks>
+		/// There are 4 possible values: first, prev, next and last.
+		/// </remarks>
 		public string Rel { get; set; }
 
+		/// <summary>
+		/// Gets or sets the page number.
+		/// </summary>
 		public int PageNumber { get; set; }
 	}
 }
