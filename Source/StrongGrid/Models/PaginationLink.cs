e07b1df1477cf2f1b110ddc28b4fc194dfb5e3ff
Jericho <desautelsj@hotmail.com>
2021-01-18 13:43:34 -0500

    (GH-361) Add a way to return pagination information that SendGrid returns in a Link' response header

---
+++ Source/StrongGrid/Models/PaginationLink.cs
@@ -0,0 +1,11 @@
+namespace StrongGrid.Models
+{
+	public class PaginationLink
+	{
+		public string Link { get; set; }
+
+		public string Rel { get; set; }
+
+		public int PageNumber { get; set; }
+	}
+}
