f3f31c04b1a783f9cffb70ee8fd7001d295c6790
Jericho <desautelsj@hotmail.com>
2021-01-25 13:10:20 -0500

    ﻿(GH-367) Fix unit tests

--- Source/StrongGrid.UnitTests/Resources/IpAddressesTests.cs
+++ Source/StrongGrid.UnitTests/Resources/IpAddressesTests.cs
@@ -168,14 +168,24 @@ namespace StrongGrid.UnitTests.Resources
 		public async Task GetAllAsync()
 		{
 			// Arrange
+			var limit = 10;
+			var offset = 0;
+			var endpoint = Utils.GetSendGridApiUri(ENDPOINT) + $"?limit={limit}&offset={offset}";
+
 			var mockHttp = new MockHttpMessageHandler();
-			mockHttp.Expect(HttpMethod.Get, Utils.GetSendGridApiUri(ENDPOINT)).Respond("application/json", MULTIPLE_IPADDRESSES_JSON);
+			mockHttp.Expect(HttpMethod.Get, endpoint).Respond((HttpRequestMessage request) =>
+			{
+				var response = new HttpResponseMessage(HttpStatusCode.OK);
+				response.Headers.Add("Link", $"<{endpoint}>; rel=\"next\"; title=\"1\", <{endpoint}>; rel=\"prev\"; title=\"1\", <{endpoint}>; rel=\"last\"; title=\"1\", <{endpoint}>; rel=\"first\"; title=\"1\"");
+				response.Content = new StringContent(MULTIPLE_IPADDRESSES_JSON);
+				return response;
+			});
 
 			var client = Utils.GetFluentClient(mockHttp);
 			var ipAddresses = new IpAddresses(client);
 
 			// Act
-			var result = await ipAddresses.GetAllAsync(false, null, 10, 0, CancellationToken.None);
+			var result = await ipAddresses.GetAllAsync(false, null, limit, offset, CancellationToken.None);
 
 			// Assert
 			mockHttp.VerifyNoOutstandingExpectation();
@@ -212,8 +222,17 @@ namespace StrongGrid.UnitTests.Resources
 		public async Task GetUnassignedAsync()
 		{
 			// Arrange
+			var limit = 10;
+			var endpoint = Utils.GetSendGridApiUri(ENDPOINT) + $"?limit={limit}&offset=0";
+
 			var mockHttp = new MockHttpMessageHandler();
-			mockHttp.Expect(HttpMethod.Get, Utils.GetSendGridApiUri(ENDPOINT) + $"?exclude_whitelabels=false&limit=500&offset=0&sort_by_direction=asc").Respond("application/json", MULTIPLE_IPADDRESSES_JSON);
+			mockHttp.Expect(HttpMethod.Get, endpoint).Respond((HttpRequestMessage request) =>
+			{
+				var response = new HttpResponseMessage(HttpStatusCode.OK);
+				response.Headers.Add("Link", $"<{endpoint}>; rel=\"next\"; title=\"1\", <{endpoint}>; rel=\"prev\"; title=\"1\", <{endpoint}>; rel=\"last\"; title=\"1\", <{endpoint}>; rel=\"first\"; title=\"1\"");
+				response.Content = new StringContent(MULTIPLE_IPADDRESSES_JSON);
+				return response;
+			});
 
 			var client = Utils.GetFluentClient(mockHttp);
 			var ipAddresses = new IpAddresses(client);
