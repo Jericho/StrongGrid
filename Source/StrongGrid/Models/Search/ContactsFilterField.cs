9ef466df3dd4c7e759e63175798d927b51731a4d
jericho <desautelsj@hotmail.com>
2022-02-04 17:52:29 -0500

    Additional contact filtering fields for segmentation.
    Resolves #409

--- Source/StrongGrid/Models/Search/ContactsFilterField.cs
+++ Source/StrongGrid/Models/Search/ContactsFilterField.cs
@@ -8,28 +8,10 @@ namespace StrongGrid.Models.Search
 	public enum ContactsFilterField
 	{
 		/// <summary>
-		/// The email address.
+		/// The additional emails associated with the contact.
 		/// </summary>
-		[EnumMember(Value = "email")]
-		EmailAddress,
-
-		/// <summary>
-		/// The email domains.
-		/// </summary>
-		[EnumMember(Value = "email_domains")]
-		EmailDomains,
-
-		/// <summary>
-		/// The first name.
-		/// </summary>
-		[EnumMember(Value = "first_name")]
-		FirstName,
-
-		/// <summary>
-		/// The last name.
-		/// </summary>
-		[EnumMember(Value = "last_name")]
-		LastName,
+		[EnumMember(Value = "alternate_emails")]
+		AlternateEmails,
 
 		/// <summary>
 		/// The address line 1.
@@ -50,10 +32,10 @@ namespace StrongGrid.Models.Search
 		City,
 
 		/// <summary>
-		/// The state/province/region.
+		/// The contact identifier.
 		/// </summary>
-		[EnumMember(Value = "state_province_region")]
-		StateOrProvice,
+		[EnumMember(Value = "contact_id")]
+		Id,
 
 		/// <summary>
 		/// The country.
@@ -62,9 +44,85 @@ namespace StrongGrid.Models.Search
 		Country,
 
 		/// <summary>
+		/// The date the contact was created.
+		/// </summary>
+		[EnumMember(Value = "created_at")]
+		CreatedOn,
+
+		/// <summary>
+		/// The email address.
+		/// </summary>
+		[EnumMember(Value = "email")]
+		EmailAddress,
+
+		/// <summary>
+		/// The email domains.
+		/// </summary>
+		[EnumMember(Value = "email_domains")]
+		EmailDomains,
+
+		/// <summary>
+		/// No documentation has been provided.
+		/// See &lt;a href="https://docs.sendgrid.com/for-developers/sending-email/segmentation-query-language#fields"&gt;SendGrid documentation&lt;a&gt;.
+		/// </summary>
+		[EnumMember(Value = "event_data")]
+		EventData,
+
+		/// <summary>
+		/// No documentation has been provided.
+		/// See &lt;a href="https://docs.sendgrid.com/for-developers/sending-email/segmentation-query-language#fields"&gt;SendGrid documentation&lt;a&gt;.
+		/// </summary>
+		[EnumMember(Value = "event_source")]
+		EventSource,
+
+		/// <summary>
+		/// No documentation has been provided.
+		/// See &lt;a href="https://docs.sendgrid.com/for-developers/sending-email/segmentation-query-language#fields"&gt;SendGrid documentation&lt;a&gt;.
+		/// </summary>
+		[EnumMember(Value = "event_timestamp")]
+		EventTimestamp,
+
+		/// <summary>
+		/// No documentation has been provided.
+		/// See &lt;a href="https://docs.sendgrid.com/for-developers/sending-email/segmentation-query-language#fields"&gt;SendGrid documentation&lt;a&gt;.
+		/// </summary>
+		[EnumMember(Value = "event_type")]
+		EventType,
+
+		/// <summary>
+		/// The first name.
+		/// </summary>
+		[EnumMember(Value = "first_name")]
+		FirstName,
+
+		/// <summary>
+		/// The lists associated with this contact.
+		/// </summary>
+		[EnumMember(Value = "list_ids")]
+		ListIds,
+
+		/// <summary>
+		/// The last name.
+		/// </summary>
+		[EnumMember(Value = "last_name")]
+		LastName,
+
+		/// <summary>
 		/// The postal code.
 		/// </summary>
 		[EnumMember(Value = "postal_code")]
 		PostalCode,
+
+		/// <summary>
+		/// The state/province/region.
+		/// </summary>
+		[EnumMember(Value = "state_province_region")]
+		StateOrProvice,
+
+		/// <summary>
+		/// The date the contact was last modified.
+		/// </summary>
+		[EnumMember(Value = "updated_at")]
+		ModifiedOn
 	}
 }
