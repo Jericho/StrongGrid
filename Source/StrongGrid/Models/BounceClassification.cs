04e2987dd6746f9b40fc1f9167983ef39dec1032
jericho <desautelsj@hotmail.com>
2023-01-09 11:19:20 -0500

    Add xml comment to indicate that we haven't been able to confirm the 7 json values

--- Source/StrongGrid/Models/BounceClassification.cs
+++ Source/StrongGrid/Models/BounceClassification.cs
@@ -7,6 +7,15 @@ namespace StrongGrid.Models
 	/// <summary>
 	/// Enumeration to indicate the type of SMTP failure.
 	/// </summary>
+	/// <remarks>
+	/// SendGrid says there are seven possible classifications but the only documented json value is 'invalid'.
+	/// As of January 2022, we haven't been able to get confirmation from SendGrid support for the other six values.
+	///
+	/// Therefore there's a high likelihood that you'll get an exception when we attempt to parse the json value.
+	/// The exception you'll get will be similar to: "There is no value in the BounceClassification enum that
+	/// corresponds to 'THE JSON VALUE'". Please notify us <a href="https://github.com/Jericho/StrongGrid/issues/477">here</a>
+	/// if you get this exception so we can update the enum.
+	/// </remarks>
 	[JsonConverter(typeof(StringEnumConverter<BounceClassification>))]
 	public enum BounceClassification
 	{
