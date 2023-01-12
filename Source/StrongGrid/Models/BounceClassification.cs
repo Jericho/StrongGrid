6895fd681e72797cb862ff0051b82dc36eaee061
jericho <desautelsj@hotmail.com>
2023-01-12 10:35:42 -0500

    Update the BounceClassification enum member values based on feedback from SendGrid support
    See: https://github.com/Jericho/StrongGrid/issues/477#issuecomment-1380424756

--- Source/StrongGrid/Models/BounceClassification.cs
+++ Source/StrongGrid/Models/BounceClassification.cs
@@ -7,58 +7,49 @@ namespace StrongGrid.Models
 	/// <summary>
 	/// Enumeration to indicate the type of SMTP failure.
 	/// </summary>
-	/// <remarks>
-	/// SendGrid says there are seven possible classifications but the only documented json value is 'invalid'.
-	/// As of January 2022, we haven't been able to get confirmation from SendGrid support for the other six values.
-	///
-	/// Therefore there's a high likelihood that you'll get an exception when we attempt to parse the json value.
-	/// The exception you'll get will be similar to: "There is no value in the BounceClassification enum that
-	/// corresponds to 'THE JSON VALUE'". Please notify us <a href="https://github.com/Jericho/StrongGrid/issues/477">here</a>
-	/// if you get this exception so we can update the enum.
-	/// </remarks>
 	[JsonConverter(typeof(StringEnumConverter<BounceClassification>))]
 	public enum BounceClassification
 	{
 		/// <summary>
 		/// Unclassified.
 		/// </summary>
-		[EnumMember(Value = "unclassified")]
+		[EnumMember(Value = "Unclassified")]
 		Unclassified,
 
 		/// <summary>
 		/// Invalid Addres.
 		/// </summary>
-		[EnumMember(Value = "invalid")]
+		[EnumMember(Value = "Invalid Address")]
 		InvalidAddress,
 
 		/// <summary>
 		/// Technical.
 		/// </summary>
-		[EnumMember(Value = "technical")]
+		[EnumMember(Value = "Technical")]
 		Technical,
 
 		/// <summary>
 		/// Content.
 		/// </summary>
-		[EnumMember(Value = "content")]
+		[EnumMember(Value = "Content")]
 		Content,
 
 		/// <summary>
 		/// Reputation.
 		/// </summary>
-		[EnumMember(Value = "reputation")]
+		[EnumMember(Value = "Reputation")]
 		Reputation,
 
 		/// <summary>
 		/// Frequency or volume.
 		/// </summary>
-		[EnumMember(Value = "frequency")] // SendGrid does not document this value. I took a wild guess.
+		[EnumMember(Value = "Frequency or Volume Too High")]
 		FrequencyOrVolume,
 
 		/// <summary>
 		/// Mailbox Unavailable.
 		/// </summary>
-		[EnumMember(Value = "mailbox")] // SendGrid does not document this value. I took a wild guess.
+		[EnumMember(Value = "Mailbox Unavailable")]
 		MailboxUnavailable,
 	}
 }
