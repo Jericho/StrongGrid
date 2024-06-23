2df047a97baffadbdb39fb89c8c85914a94f0eac
jericho <desautelsj@hotmail.com>
2024-06-23 13:47:23 -0400

    Add a few unit tests for extension methods

--- Source/StrongGrid.UnitTests/Utilities/ExtensionsTests.cs
+++ Source/StrongGrid.UnitTests/Utilities/ExtensionsTests.cs
@@ -5,78 +5,135 @@ using Xunit;
 
 namespace StrongGrid.UnitTests.Utilities
 {
-	public class ExtensionsTests
+	public class Extensions
 	{
-		[Fact]
-		public void FromUnixTime_EPOCH()
+		public class FromUnixTime
 		{
-			// Arrange
-			var unixTime = 0L;
+			[Fact]
+			public void Zero_returns_EPOCH()
+			{
+				// Arrange
+				var unixTime = 0L;
 
-			// Act
-			var result = unixTime.FromUnixTime();
+				// Act
+				var result = unixTime.FromUnixTime();
 
-			// Assert
-			result.ShouldBe(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+				// Assert
+				result.ShouldBe(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+			}
+
+			[Fact]
+			public void Seconds()
+			{
+				// Arrange
+				var unixTime = 1468155111L;
+
+				// Act
+				var result = unixTime.FromUnixTime(Internal.UnixTimePrecision.Seconds);
+
+				// Assert
+				result.ShouldBe(new DateTime(2016, 7, 10, 12, 51, 51, DateTimeKind.Utc));
+			}
+
+			[Fact]
+			public void Milliseconds()
+			{
+				// Arrange
+				var unixTime = 1719149961333L;
+
+				// Act
+				var result = unixTime.FromUnixTime(Internal.UnixTimePrecision.Milliseconds);
+
+				// Assert
+				result.ShouldBe(new DateTime(2024, 6, 23, 13, 39, 21, 333, DateTimeKind.Utc));
+			}
+
+			[Fact]
+			public void Throws_when_unknown_precision()
+			{
+				// Arrange
+				var unixTime = 1468155111L;
+				var unknownPrecision = (Internal.UnixTimePrecision)123;
+
+				// Act
+				Should.Throw<Exception>(() => unixTime.FromUnixTime(unknownPrecision));
+			}
 		}
 
-		[Fact]
-		public void FromUnixTime_2016()
+		public class ToUnixTime
 		{
-			// Arrange
-			var unixTime = 1468155111L;
+			[Fact]
+			public void EPOCH_returns_zero()
+			{
+				// Arrange
+				var date = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-			// Act
-			var result = unixTime.FromUnixTime();
+				// Act
+				var result = date.ToUnixTime();
 
-			// Assert
-			result.ShouldBe(new DateTime(2016, 7, 10, 12, 51, 51, DateTimeKind.Utc));
+				// Assert
+				result.ShouldBe(0);
+			}
+
+			[Fact]
+			public void Seconds()
+			{
+				// Arrange
+				var date = new DateTime(2016, 7, 10, 12, 51, 51, DateTimeKind.Utc);
+
+				// Act
+				var result = date.ToUnixTime(Internal.UnixTimePrecision.Seconds);
+
+				// Assert
+				result.ShouldBe(1468155111);
+			}
+
+			[Fact]
+			public void Milliseconds()
+			{
+				// Arrange
+				var date = new DateTime(2024, 6, 23, 13, 39, 21, 333, DateTimeKind.Utc);
+
+				// Act
+				var result = date.ToUnixTime(Internal.UnixTimePrecision.Milliseconds);
+
+				// Assert
+				result.ShouldBe(1719149961333);
+			}
+
+			[Fact]
+			public void Throws_when_unknown_precision()
+			{
+				// Arrange
+				var date = new DateTime(2024, 6, 23, 13, 39, 21, 333, DateTimeKind.Utc);
+				var unknownPrecision = (Internal.UnixTimePrecision)123;
+
+				// Act
+				Should.Throw<Exception>(() => date.ToUnixTime(unknownPrecision));
+			}
 		}
-		[Fact]
 
-		public void ToUnixTime_EPOCH()
+		public class ToEnum
 		{
-			// Arrange
-			var date = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			[Fact]
+			public void Throws_when_invalid_value()
+			{
+				Should.Throw<Exception>(() => "This is not a valid value".ToEnum<CampaignStatus>());
+			}
 
-			// Act
-			var result = date.ToUnixTime();
+			[Theory]
+			[InlineData("in progress", CampaignStatus.InProgress)]
+			[InlineData("IN PROGRESS", CampaignStatus.InProgress)]
+			[InlineData("In Progress", CampaignStatus.InProgress)]
+			[InlineData("In progress", CampaignStatus.InProgress)]
+			public void Is_case_insensitive(string descripion, CampaignStatus expectedStatus)
+			{
+				// Act
+				var result = descripion.ToEnum<CampaignStatus>();
 
-			// Assert
-			result.ShouldBe(0);
-		}
-
-		[Fact]
-		public void ToUnixTime_2016()
-		{
-			// Arrange
-			var date = new DateTime(2016, 7, 10, 12, 51, 51, DateTimeKind.Utc);
-
-			// Act
-			var result = date.ToUnixTime();
-
-			// Assert
-			result.ShouldBe(1468155111);
-		}
-
-		[Fact]
-		public void ToEnum_throws_when_invalid_value()
-		{
-			Should.Throw<Exception>(() => "This is not a valid value".ToEnum<CampaignStatus>());
-		}
-
-		[Theory]
-		[InlineData("in progress", CampaignStatus.InProgress)]
-		[InlineData("IN PROGRESS", CampaignStatus.InProgress)]
-		[InlineData("In Progress", CampaignStatus.InProgress)]
-		[InlineData("In progress", CampaignStatus.InProgress)]
-		public void ToEnum_is_case_insensitive(string descripion, CampaignStatus expectedStatus)
-		{
-			// Act
-			var result = descripion.ToEnum<CampaignStatus>();
-
-			// Assert
-			result.ShouldBe(expectedStatus);
+				// Assert
+				result.ShouldBe(expectedStatus);
+			}
 		}
 	}
 }
