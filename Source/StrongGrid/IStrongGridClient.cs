464f17d33c81469f7f4210be7462ec6bc941fdb8
Jericho <desautelsj@hotmail.com>
2025-01-14 11:24:06 -0500

    Allow a range of acceptable versions for certain NuGet packages
    The goal is to reduce the likelihood of version conflict when developers reference the same packages
    
    Resolves #538

---
+++ Source/StrongGrid/IStrongGridClient.cs
@@ -0,0 +1,66 @@
+using StrongGrid.Resources;
+
+namespace StrongGrid
+{
+	/// <summary>
+	/// Interface for the SendGrid REST client for SendGrid's API.
+	/// </summary>
+	public interface IStrongGridClient
+	{
+		/// <summary>
+		/// Gets the Contacts resource which allows you to manage your contacts (also sometimes refered to as 'recipients').
+		/// </summary>
+		/// <value>
+		/// The contacts.
+		/// </value>
+		IContacts Contacts { get; }
+
+		/// <summary>
+		/// Gets the CustomFields resource which allows you to manage your custom fields.
+		/// </summary>
+		/// <value>
+		/// The custom fields.
+		/// </value>
+		ICustomFields CustomFields { get; }
+
+		/// <summary>
+		/// Gets the EngagementQuality resource which allows you to retrieve metrics that define the quality of your email program.
+		/// </summary>
+		/// <value>
+		/// The engagement quality resource.
+		/// </value>
+		IEngagementQuality EngagementQuality { get; }
+
+		/// <summary>
+		/// Gets the Lists resource.
+		/// </summary>
+		/// <value>
+		/// The lists.
+		/// </value>
+		ILists Lists { get; }
+
+		/// <summary>
+		/// Gets the Segments resource.
+		/// </summary>
+		/// <value>
+		/// The segments.
+		/// </value>
+		ISegments Segments { get; }
+
+		/// <summary>
+		/// Gets the SenderIdentities resource.
+		/// </summary>
+		/// <value>
+		/// The sender identities.
+		/// </value>
+		ISenderIdentities SenderIdentities { get; }
+
+		/// <summary>
+		/// Gets the SingleSends resource which allows you to manage your single sends (AKA campaigns).
+		/// </summary>
+		/// <value>
+		/// The single sends.
+		/// </value>
+		ISingleSends SingleSends { get; }
+	}
+}
---
+++ Source/StrongGrid/StrongGridClient.cs
@@ -0,0 +1,145 @@
+using Microsoft.Extensions.Logging;
+using StrongGrid.Resources;
+using StrongGrid.Utilities;
+using System.Net;
+using System.Net.Http;
+
+namespace StrongGrid
+{
+	/// <summary>
+	/// REST client for interacting with SendGrid's API.
+	/// </summary>
+	public class StrongGridClient : BaseClient, IStrongGridClient
+	{
+		private static readonly StrongGridClientOptions _defaultOptions = new()
+		{
+			LogLevelSuccessfulCalls = LogLevel.Debug,
+			LogLevelFailedCalls = LogLevel.Error
+		};
+
+		#region PROPERTIES
+
+		/// <summary>
+		/// Gets the Contacts resource which allows you to manage your contacts (also sometimes refered to as 'recipients').
+		/// </summary>
+		/// <value>
+		/// The contacts.
+		/// </value>
+		public IContacts Contacts { get; private set; }
+
+		/// <summary>
+		/// Gets the CustomFields resource which allows you to manage your custom fields.
+		/// </summary>
+		/// <value>
+		/// The custom fields.
+		/// </value>
+		public ICustomFields CustomFields { get; private set; }
+
+		/// <inheritdoc/>
+		public IEngagementQuality EngagementQuality { get; private set; }
+
+		/// <summary>
+		/// Gets the Lists resource.
+		/// </summary>
+		/// <value>
+		/// The lists.
+		/// </value>
+		public ILists Lists { get; private set; }
+
+		/// <summary>
+		/// Gets the Segments resource.
+		/// </summary>
+		/// <value>
+		/// The segments.
+		/// </value>
+		public ISegments Segments { get; private set; }
+
+		/// <summary>
+		/// Gets the SenderIdentities resource.
+		/// </summary>
+		/// <value>
+		/// The sender identities.
+		/// </value>
+		public ISenderIdentities SenderIdentities { get; private set; }
+
+		/// <summary>
+		/// Gets the SingleSends resource which allows you to manage your single sends (AKA campaigns).
+		/// </summary>
+		/// <value>
+		/// The single sends.
+		/// </value>
+		public ISingleSends SingleSends { get; private set; }
+
+		#endregion
+
+		#region CTOR
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StrongGridClient"/> class.
+		/// </summary>
+		/// <param name="apiKey">Your SendGrid API Key.</param>
+		/// <param name="options">Options for the SendGrid client.</param>
+		/// <param name="logger">Logger.</param>
+		public StrongGridClient(string apiKey, StrongGridClientOptions options = null, ILogger logger = null)
+			: base(apiKey, null, false, options ?? _defaultOptions, logger)
+		{
+			Init();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StrongGridClient"/> class with a specific proxy.
+		/// </summary>
+		/// <param name="apiKey">Your SendGrid API Key.</param>
+		/// <param name="proxy">Allows you to specify a proxy.</param>
+		/// <param name="options">Options for the SendGrid client.</param>
+		/// <param name="logger">Logger.</param>
+		public StrongGridClient(string apiKey, IWebProxy proxy, StrongGridClientOptions options = null, ILogger logger = null)
+			: base(apiKey, new HttpClient(new HttpClientHandler { Proxy = proxy, UseProxy = proxy != null }), true, options ?? _defaultOptions, logger)
+		{
+			Init();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StrongGridClient"/> class with a specific handler.
+		/// </summary>
+		/// <param name="apiKey">Your SendGrid API Key.</param>
+		/// <param name="handler">TThe HTTP handler stack to use for sending requests.</param>
+		/// <param name="options">Options for the SendGrid client.</param>
+		/// <param name="logger">Logger.</param>
+		public StrongGridClient(string apiKey, HttpMessageHandler handler, StrongGridClientOptions options = null, ILogger logger = null)
+			: base(apiKey, new HttpClient(handler), true, options ?? _defaultOptions, logger)
+		{
+			Init();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StrongGridClient" /> class with a specific http client.
+		/// </summary>
+		/// <param name="apiKey">Your SendGrid API Key.</param>
+		/// <param name="httpClient">Allows you to inject your own HttpClient. This is useful, for example, to setup the HtppClient with a proxy.</param>
+		/// <param name="options">Options for the SendGrid client.</param>
+		/// <param name="logger">Logger.</param>
+		public StrongGridClient(string apiKey, HttpClient httpClient, StrongGridClientOptions options = null, ILogger logger = null)
+			: base(apiKey, httpClient, false, options ?? _defaultOptions, logger)
+		{
+			Init();
+		}
+
+		#endregion
+
+		#region PRIVATE METHODS
+
+		private void Init()
+		{
+			Contacts = new Contacts(FluentClient);
+			CustomFields = new CustomFields(FluentClient);
+			EngagementQuality = new EngagementQuality(FluentClient);
+			Lists = new Lists(FluentClient);
+			Segments = new Segments(FluentClient);
+			SenderIdentities = new SenderIdentities(FluentClient);
+			SingleSends = new SingleSends(FluentClient);
+		}
+
+		#endregion
+	}
+}
