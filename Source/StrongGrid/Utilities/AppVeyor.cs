509ed525926bca949fd2cfa870f3de9c4928d009
Jericho <desautelsj@hotmail.com>
2025-03-10 22:00:51 -0400

    Fix AppVeyor messages

--- Source/StrongGrid/Utilities/AppVeyor.cs
+++ Source/StrongGrid/Utilities/AppVeyor.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace StrongGrid.Utilities
 {
@@ -10,24 +8,29 @@ namespace StrongGrid.Utilities
 	/// </summary>
 	internal static class AppVeyor
 	{
-		private static HttpClient _httpClient = new HttpClient();
-		private static string _appVeyorApiUrl = Environment.GetEnvironmentVariable("APPVEYOR_API_URL");
+		private static string _appVeyorApiUrl = Environment.GetEnvironmentVariable("APPVEYOR_API_URL") ?? throw new Exception("APPVEYOR_API_URL environment variable not set");
+		private static HttpClient _httpClient;
+
+		public static HttpClient HttpClient
+		{
+			get
+			{
+				if (_httpClient == null)
+				{
+					_httpClient = new()
+					{
+						BaseAddress = new Uri(_appVeyorApiUrl)
+					};
+				}
+
+				return _httpClient;
+			}
+		}
 
 		public static void AddMessage(string message)
 		{
-			AddMessageAsync(message).GetAwaiter().GetResult();
-		}
-
-		public static async Task AddMessageAsync(string message)
-		{
-			var httpRequest = new HttpRequestMessage
-			{
-				Method = HttpMethod.Post,
-				RequestUri = new Uri($"{_appVeyorApiUrl}api/build/messages"),
-				Content = new StringContent(message)
-			};
-
-			await _httpClient.SendAsync(httpRequest, CancellationToken.None).ConfigureAwait(false);
+			var response = HttpClient.PostAsync("api/build/messages", new StringContent(message)).GetAwaiter().GetResult();
+			response.EnsureSuccessStatusCode();
 		}
 	}
 }
