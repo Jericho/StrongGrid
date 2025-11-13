3b285a971a7f9bc38bb8a0f97069bc35f070cd88
Jericho <desautelsj@hotmail.com>
2025-11-13 15:41:53 -0500

    (GH-556) Allow developers to specify the base URI

--- Source/StrongGrid/Utilities/StrongGridClientOptions.cs
+++ Source/StrongGrid/Utilities/StrongGridClientOptions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace StrongGrid.Utilities
 {
@@ -7,6 +8,8 @@ namespace StrongGrid.Utilities
 	/// </summary>
 	public class StrongGridClientOptions
 	{
+		private const string SENDGRID_V3_BASE_URI = "https://api.sendgrid.com/v3";
+
 		/// <summary>
 		/// Gets or sets the log levels for successful calls (HTTP status code in the 200-299 range).
 		/// </summary>
@@ -16,5 +19,10 @@ namespace StrongGrid.Utilities
 		/// Gets or sets the log levels for failed calls (HTTP status code outside of the 200-299 range).
 		/// </summary>
 		public LogLevel LogLevelFailedCalls { get; set; }
+
+		/// <summary>
+		/// Gets or sets the base URI of the SendGrid API endpoint.
+		/// </summary>
+		public Uri ApiEndPoint { get; set; } = new Uri(SENDGRID_V3_BASE_URI);
 	}
 }
